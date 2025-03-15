ba1b95b8a64f38302b89c162052bd53186fa9e81
gouda4352 <abhigonwar111@gmail.com>
2025-03-15 12:49:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,13 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-// const mongoose = require("mongoose")
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-// mongoose.connect("mongodb://localhost:27017/apsdb")
-// .then(()=>{console.log("mongo connected");})
-// .catch(()=>{console.log("something wrong in db");})
+app.use(cors());
+app.use(express.json());
 
-//schema , model, save(insert)
+app.post("/login", (req, res) => {
+    console.log(req.body.user)
+    res.send("login success");
+});
 
-// const userScehma = new mongoose.Schema({
-//     username:{
-//         type:String
-//     }
-// })
-
-// const User = mongoose.model("user",userScehma)
-
-app.post("/login",(req,res)=>{
-    // const username = req.body.username;
-    // const userDetails = new User
-    //({
-    //     username:username
-    res.send("success")
-    //})
-    // userDetails.save()
-    // .then(()=>{res.send("success")})
-    // .catch(()=>{res.send("didnt save")})
-    
-})
-
-app.listen(3000,(req,res)=>{
-    console.log("server started");
-})
+app.listen(3000, () => console.log("server started"));
