ec405c3f0d4cb27d038c2733a6c9d83e4923fa10
gouda4352 <abhigonwar111@gmail.com>
2025-03-15 12:15:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+// const mongoose = require("mongoose")
+app.use(express.json())
+app.use(cors())
+
+// mongoose.connect("mongodb://localhost:27017/apsdb")
+// .then(()=>{console.log("mongo connected");})
+// .catch(()=>{console.log("something wrong in db");})
+
+//schema , model, save(insert)
+
+// const userScehma = new mongoose.Schema({
+//     username:{
+//         type:String
+//     }
+// })
+
+// const User = mongoose.model("user",userScehma)
+
+app.post("/login",(req,res)=>{
+    // const username = req.body.username;
+    // const userDetails = new User
+    //({
+    //     username:username
+    res.send("success")
+    //})
+    // userDetails.save()
+    // .then(()=>{res.send("success")})
+    // .catch(()=>{res.send("didnt save")})
+    
+})
+
+app.listen(3000,(req,res)=>{
+    console.log("server started");
+})
